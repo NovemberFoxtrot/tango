78c1b016c02e13cb8a959136d1eac3fe55b3bab0
NovemberFoxtrot <november@foxtrot>
2013-07-28 20:52:37 -0700

    First dance.

---
+++ tango_test.go
@@ -0,0 +1,21 @@
+package tango
+
+import (
+	"tango"
+  "fmt"
+)
+
+func Example200OK() {
+	fmt.Println(tango.FetchMeaning("200"))
+  // Output: OK
+}
+
+func Example500InternalServerError() {
+	fmt.Println(tango.FetchMeaning("500"))
+  // Output: Internal Server Error
+}
+
+func Example000Unknown() {
+	fmt.Println(tango.FetchMeaning("000"))
+  // Output: Unknown
+}
